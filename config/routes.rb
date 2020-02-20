dbfd4b78bb674f6743815516ea86429c441b6517
Tereza Valerova <terezavalerova@gmail.com>
2020-02-20 14:16:59 +0100

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails do
-    resources :doses, only: [ :index, :new, :create, :destroy ]
+  root to: 'cocktails#home'
+
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create, :destroy ]
   end
-  resources :doses, only: [ :show, :edit, :update ]
 end
