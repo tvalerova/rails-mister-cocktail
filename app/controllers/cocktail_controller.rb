1ff3605ea43e3d705139018144c8c983f34b9843
Tereza Valerova <terezavalerova@gmail.com>
2020-02-20 14:09:45 +0100

    cocktails controller

--- app/controllers/cocktail_controller.rb
+++ app/controllers/cocktail_controller.rb
@@ -1,17 +1,22 @@
 class CocktailController < ApplicationController
   def index
-
-  end
-
-  def show
-
+    @cocktails = Cocktail.all
   end
 
   def new
+    @cocktail = Cocktail.new
+  end
 
+  def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def create
-
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 end
